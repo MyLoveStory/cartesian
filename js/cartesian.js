1ee7e7a0574b75aaa9e320ba283a295358fc159a
Luis <mikeylopez@gmail.com>
2014-01-16 23:47:06 -0500

    x and y axis main lines; getheight and width funcs

--- js/cartesian.js
+++ js/cartesian.js
@@ -2,15 +2,15 @@ var Plane = (function(){
 
   var cart = document.getElementById('cart');
   var plane2d = cart.getContext('2d'); 
+  var planeHeight = cart.height;
+  var planeWidth  = cart.width;
 
-  var drawXY = function(xLines, yLines){
+  var drawXY = function(xLines, yLines, xyColor){
    
     var i, j = i = 1;
-    var planeHeight = cart.height;
-    var planeWidth  = cart.width; 
-    var yLineInterval = parseInt(planeWidth / yLines); 
-    var xLineInterval = parseInt(planeHeight / xLines);
-
+    var yLineInterval = Math.floor(parseInt(planeWidth / yLines)); 
+    var xLineInterval = Math.floor(parseInt(planeHeight / xLines));
+    
     plane2d.beginPath();
     
     for( i; i <= planeWidth; i += yLineInterval){ 
@@ -29,17 +29,56 @@ var Plane = (function(){
       
     }
       
-      plane2d.strokeStyle = '#aaa'; 
-      plane2d.stroke();  
-   
+      plane2d.strokeStyle = xyColor; 
+      plane2d.stroke();
+      plane2d.closePath();
+      
+  }
+    
+  var drawYAxis = function(yAxisWidth){
+
+      plane2d.beginPath(); 
+      plane2d.moveTo(planeWidth / 2, 0);
+      plane2d.lineTo(planeWidth / 2, planeHeight);
+      plane2d.lineWidth = parseInt(yAxisWidth); 
+      plane2d.stroke();
+
+   }
+
+  var drawXAxis = function(xAxisWidth){
+
+      plane2d.beginPath(); 
+      plane2d.moveTo(0, planeHeight / 2);
+      plane2d.lineTo(planeWidth, planeHeight / 2);
+      plane2d.lineWidth = parseInt(xAxisWidth); 
+      plane2d.stroke();
+
+   }
+
+  var getWidth = function(){
+      
+      return planeWidth;
+  } 
+
+  var getHeight = function(){
+    
+      return planeHeight;
   }
 
   return {
-   drawXY : drawXY 
+   drawXY     : drawXY,
+   drawYAxis  : drawYAxis,
+   drawXAxis  : drawXAxis,
+   getWidth   : getWidth,
+   getHeight  : getHeight
   };
 
 
 })();
 
 
-Plane.drawXY(10, 10);
+Plane.drawXY(20, 20, '#aaa');
+Plane.drawYAxis(5);
+Plane.drawXAxis(5);
+
+
