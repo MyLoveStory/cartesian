8cbc9c6f583437dc7c8036bfc56a9ab0ef26db24
Luis <mikeylopez@gmail.com>
2014-01-18 23:30:19 -0500

    x plus quadrant

--- js/cartesian.js
+++ js/cartesian.js
@@ -1,10 +1,16 @@
-var Plane = (function(){
+var Plane = (function(canvas){
 
-  var cart = document.getElementById('cart');
+  var cart = document.getElementById(canvas);
   var plane2d = cart.getContext('2d'); 
   var planeHeight = cart.height;
   var planeWidth  = cart.width;
-
+  var planeCenter = planeWidth / 2;
+  var planeXFontPadding = (planeCenter + 5)
+  var planeXYFontPadding = (planeCenter + 10)
+  var xPlaneFontPadding  = 10;
+  var xPlaneNumberStart = 1;
+  var xPlaneNumberInterval = 1;
+  
   var drawXY = function(xLines, yLines, xyColor){
    
     var i, j = i = 0;
@@ -13,24 +19,28 @@ var Plane = (function(){
     
     plane2d.beginPath();
     
-    for( i; i <= planeWidth; i += yLineInterval){ 
-       
+    for( i; i <= planeWidth; i += xLineInterval){ 
+      
       plane2d.moveTo(i, 0);
       plane2d.lineTo(i, planeHeight);
-
+       
+      if( i > planeCenter ){ 
+         
+         this.drawText(i - xPlaneFontPadding, planeXYFontPadding, xPlaneNumberStart, 'Arial', '#f00', 10);
+         xPlaneNumberStart += xPlaneNumberInterval;
+      } 
     }
 
-    for( j; j <= planeHeight; j += xLineInterval){
+    for( j; j <= planeHeight; j += yLineInterval){
 
       plane2d.moveTo(0, j);
       plane2d.lineTo(planeWidth, j);
-      
     }
       
       plane2d.strokeStyle = xyColor; 
-      plane2d.stroke();
-      plane2d.closePath();
-      
+      plane2d.stroke(); 
+      plane2d.closePath(); 
+
   }
     
   var drawYAxis = function(yAxisWidth, color){
@@ -42,7 +52,7 @@ var Plane = (function(){
       plane2d.strokeStyle = color;
       plane2d.stroke();
 
-   }
+  }
 
   var drawXAxis = function(xAxisWidth, color){
 
@@ -69,7 +79,13 @@ var Plane = (function(){
       plane2d.fill();
   }
 
+  var drawText = function(x, y, text, font, color, width){
+     
+      plane2d.font = font; 
+      plane2d.fillStyle = color;
+      plane2d.fillText(text, x, y, width); 
 
+  }
 
   var getWidth = function(){
       
@@ -87,17 +103,17 @@ var Plane = (function(){
    drawXAxis  : drawXAxis,
    drawRect   : drawRect,
    drawCircle : drawCircle,
+   drawText   : drawText,
    getWidth   : getWidth,
    getHeight  : getHeight
   };
 
-
-})();
-
+})('cart');
 
 Plane.drawXY(10, 10, '#ccc');
 Plane.drawYAxis(2, '#aaa');
 Plane.drawXAxis(2, '#aaa');
 Plane.drawCircle(200, 200, 2, 0, 2*Math.PI, '#000');
+//Plane.drawText(205, 210, '1', 'Arial', '#f00', 10); 
 
 
